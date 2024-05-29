353aaafa30d8efa64d9966550c5c0de2e537b8e7
deniskayader2004 <makm0411@gmail.com>
2024-05-29 12:15:51 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lr.h"
+int main(){
+    int choise;
+    scanf("%d", &choise);
+    switch(choise){
+    case(1): return lab1();
+    case(11):return lab11();
+    case(2): return lab2();
+    case (21): return lab21();
+    case(3): return lab3();
+    case(31): return lab31();
+    case(4): return lab4();
+    case(41): return lab41();
+    case(5): return lab5();
+    case(51): return lab51();
+    case(6): return lab6();
+    case(7): return lab7();
+        }
+return 0;
+}
